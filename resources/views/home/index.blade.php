bfd06d5cdd936a4b3555f37145641dbabef1b09f
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-02-24 16:47:21 +0700

    blade

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+<div class="">
+    hello iam blade template !
+</div>
+
+@isset($name)
+ <div>the name is {{ $name }}</div>
+@endisset
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home/index', [
+        'name'=> 'faisal',
+    ] );
+});
+
+/** get */
+/** post */
+/** put */
+/** delete */
+/** get */
