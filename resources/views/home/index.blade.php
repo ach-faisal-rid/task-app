d9dedabe045ca3e5409fa3371a6d18ecb6c9afa0
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-02-24 17:11:54 +0700

    blade directives

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,18 @@
-<div class="">
+<div>
     hello iam blade template !
 </div>
 
 @isset($name)
  <div>the name is {{ $name }}</div>
 @endisset
+<br>
+<div>
+    <!--dd($tasks)-->
+    @if (is_array($tasks) && count($tasks))
+        @foreach ($tasks as $task)
+            <div>{{$task->title}}</div>
+        @endforeach()
+    @else
+        <div>There are no tasks!</div>
+    @endif
+</div>
