428526dbd48480ab6d1ea47277fcd594b3a1ed18
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-02-26 15:02:47 +0700

    session, error flash message

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,30 +1,47 @@
 @extends('layouts.app')
 
-@section('title', 'add tasks')
+@section('title', 'Add Task')
 
 @section('content')
-  <form method="POST" action="{{ route('tasks.store') }}">
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form method="POST" action="{{ route('tasks.store') }}">
     @csrf
 
     <div>
-      <label for="title">
-        Title
-      </label>
-      <input text="text" name="title" id="title" />
+        <label for="title">
+            Title
+        </label>
+        <input type="text" name="title" id="title" value="{{ old('title') }}" />
     </div>
 
     <div>
-      <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5"></textarea>
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
     </div>
 
     <div>
-      <label for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10"></textarea>
+        <label for="long_description">Long Description</label>
+        <textarea name="long_description" id="long_description" rows="10">{{ old('description') }}</textarea>
     </div>
 
     <div>
-      <button type="submit">Add Task</button>
+        <button type="submit">Add Task</button>
     </div>
-  </form>
+</form>
+
+@if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
 @endsection
